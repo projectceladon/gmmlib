5f83bb31e5f5a181da33533eadc2997a9af94ec5
kalle <kalyan.alle@intel.com>
2022-06-27 22:03:40 +0530

    Adding ADL-N support for GMMLib
    Adding support for ADL-N as per the upstream gmmlib
    
    Tracked-On: OAM-105467
    Signed-off-by: Kalyan, Alle <kalyan.alle@intel.com>
    Change-Id: I3b9784c013c397bede979b9186cf74c1a9efd569

--- Source/GmmLib/Platform/GmmGen12Platform.cpp
+++ Source/GmmLib/Platform/GmmGen12Platform.cpp
@@ -309,6 +309,7 @@ FCRECTALIGN(TILE__64_2D_128bpe, 128,  32,  32,   32, 32);
     Data.HighestAcceptablePhysicalAddress = GFX_MASK_LARGE(0, 38);
     if(GFX_GET_CURRENT_PRODUCT(Data.Platform) == IGFX_ALDERLAKE_S ||
 	(GFX_GET_CURRENT_PRODUCT(Data.Platform) == IGFX_ALDERLAKE_P) ||
+	(GFX_GET_CURRENT_PRODUCT(Data.Platform) == IGFX_ALDERLAKE_N) ||
 	(GFX_GET_CURRENT_PRODUCT(Data.Platform) == IGFX_RAPTORLAKE_S) ||
 	(GFX_GET_CURRENT_PRODUCT(Data.Platform) >= IGFX_XE_HP_SDV))
     {
